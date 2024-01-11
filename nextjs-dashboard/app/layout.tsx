c4e2b95bc38f5b4b4af91f978ea772a457cfb8c5
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-11 17:09:19 +0000

    nextjs tutorial

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,15 @@
+// import global.css. THis adds CSS rules to all routes in application, can add this to any component, but good practice to add to top level. Next,js this is root layout
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
