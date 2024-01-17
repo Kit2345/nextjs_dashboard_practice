3fe5f3df973f8d1910d5207a94af9f46599dd5cc
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-17 15:33:57 +0000

    added metadata for invoices page

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,6 +1,16 @@
 // import global.css. THis adds CSS rules to all routes in application, can add this to any component, but good practice to add to top level. Next,js this is root layout
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The offical Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-practice-pi.vercel.app'),
+};
 
 export default function RootLayout({
   children,
